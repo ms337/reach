9a75d8fd7f0d335df0c115a5e5ce3abab9bed81c
akulbector <40412673+akulbector@users.noreply.github.com>
2018-07-12 21:34:27 -0400

    Create serializers.py
    Used in views to convert model instances to and from json

---
+++ rides/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Place, Profile, RideRequest, RidePosting
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = ('city')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('email','name','user','age','rating','num_ratings','num_rides','friendlist','phone_num')
+
+class RideRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideRequest
+        fields = ('__all__')
+
+class RidePostingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RidePosting
+        fields = ('__all__')
