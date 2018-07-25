a00df8fb055153252ae3f990f67c8095480e120a
ms337 <msinghal@uwo.ca>
2018-07-24 20:55:48 -0400

    Fixed editable param in models.py

--- rides/models.py
+++ rides/models.py
@@ -33,12 +33,12 @@ class Profile(models.Model):
         return self.name + 'with email: ' + self.email
 
 class RidePosting(models.Model):
-    dest = models.ForeignKey(Place,related_name="rides_from",on_delete=models.CASCADE, editable = False)
-    start =  models.ForeignKey(Place, related_name="rides_to",on_delete=models.CASCADE,editable=False)
+    dest = models.ForeignKey(Place,related_name="rides_from",on_delete=models.CASCADE)
+    start =  models.ForeignKey(Place, related_name="rides_to",on_delete=models.CASCADE)
 
-    owner = models.ForeignKey(User,related_name = "RidePosts",on_delete=models.CASCADE, editable = False)
+    owner = models.ForeignKey(User,related_name = "RidePosts",on_delete=models.CASCADE)
 
-    date = models.DateField(db_index=True, editable = False)
+    date = models.DateField(db_index=True)
     time_min = models.TimeField(db_index=True)
     time_max = models.TimeField(db_index=True, default=time_min)
 
@@ -50,7 +50,7 @@ class RidePosting(models.Model):
     confirmed_riders = models.ManyToManyField(User, related_name = "Rides", blank = True)
     potential_riders = models.ManyToManyField(User, related_name = "Ride_Offers", blank = True)
 
-    stops = ArrayField(base_field=models.CharField(max_length=30), size = 100,blank = True, editable = False)
+    stops = ArrayField(base_field=models.CharField(max_length=30), size = 100,blank = True)
 
     #objects = RidePostingManager()
 
@@ -69,7 +69,7 @@ class RideRequest(models.Model):
 
     #timeInRange = (models.TimeField, models.TimeField)
     seatsNeeded = models.IntegerField(default= 1)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name= "The rider taking the ride", related_name= "riderrequests",editable=False) # check related_name. Is supposed to create a reverse relationship where riderrequests.all will return all rider all instances of RideRequests it is linked to.
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name= "The rider taking the ride", related_name= "riderrequests") # check related_name. Is supposed to create a reverse relationship where riderrequests.all will return all rider all instances of RideRequests it is linked to.
     #flexibleTime = models.BooleanField(default= False)
     #currentlyRequestedList = models.ManyToManyField(null=True, blank=True, symmetrical= False)
     requestCompleted = models.BooleanField(default=False)
