91a6111d6eb5897a4fcd1b70710c2916b91d5d0d
akulbector <40412673+akulbector@users.noreply.github.com>
2018-07-24 20:09:04 -0400

    Update urls.py

--- rides/urls.py
+++ rides/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.routers import DefaultRouter
 from .import views
 
 
-router = DefaultRouter()
-router.register(r'ridepostings', views.RidePostingReadOnlyViewSet)
 # API endpoints
 urlpatterns = format_suffix_patterns([
     url(r'^ridepostings/create/$',
@@ -14,5 +10,6 @@ urlpatterns = format_suffix_patterns([
         views.RidePostingRUDAPIView.as_view()),
     url(r'^ridepostings/(?P<pk>[0-9]+)/accept/$',
         views.RidePostingAcceptView.as_view()),
-    url(r'^', include(router.urls))
+    url(r'^ridepostings/$',
+        views.RidePostingListView.as_view()),
 ])
